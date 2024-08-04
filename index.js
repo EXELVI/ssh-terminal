d4b13cce63f27f026e904b565184c5d6a758339b
EXELVI <exelvi@outlook.com>
2024-08-04 21:29:20 +0200

    feat: Added commands, bug fix, ecc

--- index.js
+++ index.js
@@ -17,14 +17,21 @@ let users = []
 const fileSystemFunctions = {
     createFile: function (userDB, path, content = '',) {
         const parentDir = navigateToPath(path, true);
-        parentDir[getFileName(path)] = content;
+        parentDir[getFileName(path)] = { content: content, owner: userDB.uid, size: content.length, lastModified: new Date(), created: new Date(), type: 'file' };
         userDB.stats.files++;
+    },
+    createDirectory: function (userDB, path) {
+        const parentDir = navigateToPath(path, true);
+        parentDir[getFileName(path)] = { owner: userDB.uid, lastModified: new Date(), created: new Date(), type: 'directory', content: {} };
+        userDB.stats.directories++;
     },
     changeFileContent: function (path, content) {
         const file = navigateToPath(path);
-        if (typeof file === 'string') {
+        if (file.type == 'file') {
             const parentDir = navigateToPath(path, true);
-            parentDir[getFileName(path)] = content;
+            parentDir[getFileName(path)].content = content;
+            parentDir[getFileName(path)].size = content.length;
+            parentDir[getFileName(path)].lastModified = new Date();
         } else {
             return false
         }
@@ -39,7 +46,7 @@ const fileSystemFunctions = {
             this.createFile(userDB, `${userDB.home}/.bash_history`);
             userDir = navigateToPath(userDB.home);
         }
-        return userDir[".bash_history"];
+        return navigateToPath(`${userDB.home}/.bash_history`, true)["content"];
     },
     addToBashHistory: function (userDB, command) {
         var userDir = navigateToPath(userDB.home);
@@ -47,13 +54,14 @@ const fileSystemFunctions = {
             this.createFile(userDB, `${userDB.home}/.bash_history`);
             userDir = navigateToPath(userDB.home);
         }
-        userDir[".bash_history"] += command + "\n";
+        console.log(userDir)
+        navigateToPath(`${userDB.home}/.bash_history`, true)["content"] += command + "\n";
     },
     tree: function (path, indent = '') {
         let treeOutput = '';
         const target = navigateToPath(path);
 
-        if (typeof target !== 'object') {
+        if (target.type !== 'directory') {
             throw new Error('Path not found');
         }
 
@@ -61,7 +69,7 @@ const fileSystemFunctions = {
         keys.forEach((key, index) => {
             const isLast = index === keys.length - 1;
             treeOutput += indent + (isLast ? '└── ' : '├── ') + key + '\n';
-            if (typeof target[key] === 'object') {
+            if (target[key].type === 'directory') {
                 treeOutput += this.tree(`${path}/${key}`, indent + (isLast ? '    ' : '│   '));
             }
         });
@@ -69,13 +77,29 @@ const fileSystemFunctions = {
         return treeOutput;
     },
     readFileContent: function (path) {
-
         const file = navigateToPath(path);
-        if (typeof file === 'string') {
-            return file;
+        if (file && file.type === 'file') {
+            return file.content;
         } else {
             return false;
         }
+    },
+    copy: function (source, destination) {
+        const sourceObj = navigateToPath(source);
+        if (sourceObj === false) {
+            throw new Error('Source path not found');
+        }
+        const destinationDir = navigateToPath(destination, true);
+        if (destinationDir === false) {
+            throw new Error('Destination path not found');
+        }
+        console.log(destinationDir)
+        destinationDir[getFileName(destination)] = { ...sourceObj };
+        console.log(destinationDir)
+    },
+    move: function (source, destination) {
+        this.copy(source, destination);
+        this.remove(source);
     },
 };
 
@@ -83,12 +107,14 @@ const fileSystemFunctions = {
 function navigateToPath(path, parent = false) {
     const parts = path.split('/').filter(part => part.length > 0);
     let current = fileSystem['/'];
-    for (let i = 0; i < (parent ? parts.length - 1 : parts.length); i++) {
-        current = current[parts[i]];
-        if (current === undefined) {
-            return false
+   for (let i = 0; i < (parent ? parts.length - 1 : parts.length); i++) {
+        const part = parts[i];
+        if (current[part] === undefined) {
+            return false;
         }
+        current = current[part].content;
     }
+
     return current;
 }
 
@@ -99,6 +125,7 @@ function getFileName(path) {
 
 
 const db = require('./db');
+const { create } = require('domain');
 
 const key = generateKeyPairSync('rsa', {
     bits: 2048
@@ -168,14 +195,7 @@ const server = new Server({
             execute: function () {
                 var output = 'Available commands:\r\n';
 
-
                 commands.sort((a, b) => a.name.localeCompare(b.name)).forEach(command => {
-                    /*table:
-                    name    description (up to termianl width, after split, go to next line)
-                            (next line) description (still up to terminal width)
-                            ecc
-                            
-                    */
                     var descriptionLines = []
                     var description = command.description;
                     while (description.length > PTY.cols - 10) {
@@ -188,13 +208,126 @@ const server = new Server({
                     descriptionLines.slice(1).forEach(line => {
                         output += ' '.repeat(10) + line + '\r\n';
                     })
-                    console.log(output)
-                    return output;
-
                 })
+
+                console.log(output)
+                return output;
 
             }
         },
+        {
+            name: "clear",
+            description: "Clear the screen",
+            root: false,
+            execute: function () {
+                return '\x1Bc';
+            }
+        },
+        {
+            name: 'date',
+            description: 'Display the current date and time',
+            root: false,
+            execute: function () {
+                return new Date().toLocaleString() + '\r\n';
+            }
+        },
+        {
+            name: "touch",
+            description: "Create a new file",
+            root: false,
+            execute: function (input) {
+                const fileName = input.split(' ')[1];
+                if (fileName) {
+                    fileSystemFunctions.createFile(`${currentDir}/${fileName}`, fileName == "grass.txt" ? "Time to go outside :)" : "");
+                    return '';
+                } else {
+                    return 'touch: missing file operand\r\n';
+                }
+            }
+        },
+        {
+            name: 'rm',
+            root: false,
+            description: 'Delete a file or directory',
+            execute: function (input) {
+                const path = input.split(' ')[1];
+                try {
+                    fileSystemFunctions.remove(`${currentDir}/${path}`);
+                    return '';
+                } catch (error) {
+                    return `rm: ${path}: ${error.message}`;
+                }
+            }
+        },
+        {
+            name: 'cat',
+            root: false,
+            description: 'Display the content of a file',
+            execute: function (input) {
+                const fileName = input.split(' ')[1];
+                try {
+                    return fileSystemFunctions.readFileContent(`${currentDir}/${fileName}`) + '\r\n';
+                } catch (error) {
+                    return `cat: ${fileName}: No such file or directory\r\n`;
+                }
+            }
+        },
+        {
+            name: 'echo',
+            root: false,
+            description: 'Print text to the terminal',
+            execute: function (input) {
+                var parts = input.split(' ');
+                if (parts[parts.length - 2] === '>') {
+                    parts.pop();
+                    parts.pop();
+                }
+                parts.shift();
+                return parts.join(' ') + '\r\n';
+            }
+        },
+        {
+            name: 'ls',
+            root: false,
+            description: 'List files and directories',
+            execute: function (input) {
+                const params = input.split(' ')
+                const currentDirContent = navigateToPath(currentDir);
+                if (params[2]) {
+                    const target = navigateToPath(`${currentDir}/${params[2]}`);
+                    if (target) currentDirContent = target;
+                } else if (params[1]) {
+                    const target = navigateToPath(`${currentDir}/${params[1]}`);
+                    if (target) currentDirContent = target;
+                }
+
+                if (params[1] == "-la") {
+                    var output = 'total ' + Object.keys(currentDirContent).length + '\r\n';
+                    Object.keys(currentDirContent).forEach((key) => {
+                        output += `${currentDirContent[key].type == 'directory' ? 'd' : '-'}rwxr-xr-x 1 ${currentDirContent[key].owner} ${currentDirContent[key].owner} ${currentDirContent[key].size} ${currentDirContent[key].lastModified} ${key}\r\n`;
+                    });
+                    return output;
+                } else {
+                }
+                
+            },
+        },
+        {
+            name: "eval",
+            description: "Evaluate JavaScript code",
+            root: false,
+            execute: function (input) {
+                var code = input.split(' ').slice(1).join(' ');
+                try {
+                    return inspect(eval(code)) + '\r\n';
+                }
+                catch (error) {
+                    return error.message + '\r\n';
+                }
+            }
+        }
+
+
     ]
 
     client.on('authentication', (ctx) => {
@@ -203,7 +336,12 @@ const server = new Server({
         userDB = users.find(user => user.username === ctx.username);
         if (!userDB) {
             user = { username: ctx.username, password: "", home: "/home/" + ctx.username, uid: 1000 + users.length, groups: [ctx.username], stats: { commands: {}, files: 0, directories: 0, sudo: 0, uptime: 0, lastLogin: new Date() } }
-            userDB = user
+            userDB = user;
+            users.push(user);
+            console.log(navigateToPath("/etc/skel"))
+            if (navigateToPath("/etc/skel")) {
+                fileSystemFunctions.copy("/etc/skel", userDB.home);
+            }
         }
         currentDir = userDB.home;
         if (userDB.password) {
@@ -253,9 +391,8 @@ const server = new Server({
                 }
 
                 var motd = ""
-                console.log(fileSystem)
-                if (fileSystem["/"].etc?.motd) {
-                    motd = fileSystem["/"].etc.motd
+                if (navigateToPath("/etc/motd")) {
+                    motd = fileSystemFunctions.readFileContent("/etc/motd");
                 }
                 motd.split('').forEach((char, index) => {
                     setTimeout(() => {
@@ -283,8 +420,10 @@ const server = new Server({
                         shell.write('^C');
                         input = '';
                     } else if (data.toString() === '\u007F') {
-                        shell.write('\b \b');
-                        input = input.slice(0, -1);
+                        if (input.length > 0) {
+                            input = input.slice(0, -1);
+                            shell.write('\b \b');
+                        }
                     } else input += data;
                 });
 
@@ -297,7 +436,7 @@ const server = new Server({
                     } catch (error) {
                         console.log(error)
                     }
-                    output += `${userDB.uid == 0 ? '\x1B[31m' : '\x1B[32m'}${userDB.username}@${instanceName}\x1B[0m:\x1B[34m${currentDir}\x1B[0m${userDB.uid == 0 ? '#' : '$'} `;
+                    output += `${userDB.uid == 0 ? '\x1B[31m' : '\x1B[32m'}${userDB.username}@${instanceName}\x1B[0m:\x1B[34m${currentDir}\x1B[0m${userDB.uid == 0 ? '#' : '$'} ${input}\r\n`;
 
                     if (out) {
                         shell.write(output);
@@ -346,7 +485,7 @@ const server = new Server({
 
                     if (command) {
                         userDB.stats.commands[command.name] = userDB.stats.commands[command.name] ? userDB.stats.commands[command.name] + 1 : 1;
-                        var out = command.execute(input.split(' ').slice(1));
+                        var out = command.execute(input, currentUser);
                         if (!out) return;
 
                         var inputParts = input.split(' ');
@@ -366,8 +505,13 @@ const server = new Server({
                             shell.write(out);
                         }
                     } else {
-                        shell.write(`${input.split(' ')[0]}: command not found`);
+                        if (input !== "") {
+                            shell.write(`${input.split(' ')[0]}: command not found \r\n`);
+                        }
                     }
+
+                    shell.write(`${userDB.uid == 0 ? '\x1B[31m' : '\x1B[32m'}${userDB.username}@${instanceName}\x1B[0m:\x1B[34m${currentDir}\x1B[0m${userDB.uid == 0 ? '#' : '$'} `);
+
                 }
 
             });
